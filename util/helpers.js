074b2c6eac087e21057040897c372c037a4a6732
Prateek Jaipuria <jaipuria@tau.isi.edu>
2015-09-09 18:16:51 -0700

    changed file permissions

--- util/helpers.js
+++ util/helpers.js
@@ -42,3 +42,16 @@ function plot(localDBName, recordFetchLimit) {
 		requestData();
 	}
 }
+
+//First, checks if it isn't implemented yet.
+if (!String.prototype.format) {
+  String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) { 
+      return typeof args[number] != 'undefined'
+        ? args[number]
+        : match
+      ;
+    });
+  };
+}
